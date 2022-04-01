50b837a6bee504a543925a6f1a1c4e4178521017
Expired <imoses2@hotmail.com>
2022-03-31 20:00:53 -0700

    login/out works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,14 +30,6 @@ router.get('/', (req, res) => {
 		});
 });
 
-router.get('/login', (req, res) => {
-  // res.render('login');
-});
-
-router.get('/register', (req, res) => {
-	res.render('register');
-});
-
 router.get('/deck/:id', (req, res) => {
 	Deck.findOne({
 		where: {
@@ -87,72 +79,14 @@ router.get('/deck/:id', (req, res) => {
 			console.log(err);
 			res.status(500).json(err);
 		});
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-  // Post.findOne({
-    // where: {
-      // id: req.params.id
-    // },
-    // attributes: [
-      // 'id',
-      // 'post_url',
-      // 'title',
-      // 'created_at',
-      // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    // ],
-    // include: [
-      // {
-        // model: Comment,
-        // attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        // include: {
-          // model: User,
-          // attributes: ['username']
-        // }
-      // },
-      // {
-        // model: User,
-        // attributes: ['username']
-      // }
-    // ]
-  // })
-    // .then(dbPostData => {
-      // if (!dbPostData) {
-        // res.status(404).json({ message: 'No post found with this id' });
-        // return;
-      // }
+});
 
-      // serialize the data
-      // const post = dbPostData.get({ plain: true });
+router.get('/login', (req, res) => {
+  // res.render('login');
+});
 
-      // pass data to template
-      // res.render('single-post', { 
-				// post,
-				// loggedIn:	req.session.loggedIn
-			// });
-    // })
-    // .catch(err => {
-      // console.log(err);
-      // res.status(500).json(err);
-    // });
+router.get('/register', (req, res) => {
+	res.render('register');
 });
 
 module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,41 @@
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#login-email').value.trim();
+  const password = document.querySelector('#login-password').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.reload();
+      // document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('#logout').addEventListener('click', logout);
