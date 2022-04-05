b2d78a5baa7bd6e6086103f96a1422e835a77c52
Expired <imoses2@hotmail.com>
2022-04-04 17:45:37 -0700

    dashboard done

--- controllers/html/home-routes.js
+++ controllers/html/home-routes.js
@@ -88,7 +88,7 @@ router.get('/deck/:id', (req, res) => {
 
 router.get('/login', (req, res) => {
 	console.log('login route');
-  // res.render('login');
+  res.render('register');
 });
 
 router.get('/register', (req, res) => {
